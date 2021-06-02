ae9fcc65c136e7889ea21f202ffb132c533aaf8b
daluisgarcia <daluisgarcia@gmail.com>
2021-06-02 13:38:41 -0400

    Services and DTOs added

---
+++ src/dtos/customers.dto.ts
@@ -0,0 +1,28 @@
+import {
+    IsString,
+    IsNumber,
+    IsUrl,
+    IsNotEmpty,
+    IsEmpty,
+    IsPositive,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateCustomerDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
+    @IsString()
+    readonly description: string;
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
+    readonly price: number;
+    @IsNumber()
+    @IsNotEmpty()
+    readonly stock: number;
+    @IsUrl()
+    @IsEmpty()
+    readonly image: string;
+}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
